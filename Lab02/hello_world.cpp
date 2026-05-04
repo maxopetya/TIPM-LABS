1b1c2b020128ac08de468ac0a24edb5ba753c957
maxopetya <maxopetya@proton.me>
2026-05-05 00:22:34 +0300

    Lab02: apply clang-format -style=Mozilla (Part III)

--- Lab02/hello_world.cpp
+++ Lab02/hello_world.cpp
@@ -2,12 +2,14 @@
 #include <iostream>
 #include <string>
 
-int main() {
-    std::string name;
-    // Считываем имя пользователя из стандартного потока ввода.
-    std::cout << "Enter your name: ";
-    std::getline(std::cin, name);
-    // Выводим приветствие в стандартный поток вывода.
-    std::cout << "Hello world from " << name << std::endl;
-    return 0;
+int
+main()
+{
+  std::string name;
+  // Считываем имя пользователя из стандартного потока ввода.
+  std::cout << "Enter your name: ";
+  std::getline(std::cin, name);
+  // Выводим приветствие в стандартный поток вывода.
+  std::cout << "Hello world from " << name << std::endl;
+  return 0;
 }
