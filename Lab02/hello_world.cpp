20f5452eb31bd2c5deb62c97734d0c6a2935c513
maxopetya <maxopetya@proton.me>
2026-05-05 00:21:32 +0300

    Lab02: add inline comments (Part II, step 6-7)

--- Lab02/hello_world.cpp
+++ Lab02/hello_world.cpp
@@ -1,10 +1,13 @@
+// Программа выводит приветствие пользователю по введённому имени.
 #include <iostream>
 #include <string>
 
 int main() {
     std::string name;
+    // Запрашиваем имя у пользователя.
     std::cout << "Enter your name: ";
     std::getline(std::cin, name);
+    // Печатаем приветствие в стандартный поток вывода.
     std::cout << "Hello world from " << name << std::endl;
     return 0;
 }
