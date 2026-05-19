e9a55eb2a8dfcf095685e92cf4f3e140801b4d47
maxopetya <maxopetya@proton.me>
2026-05-20 01:49:08 +0300

    hunter

--- Lab07/demo/main.cpp
+++ Lab07/demo/main.cpp
@@ -1,7 +1,20 @@
-#include <mylog/mylog.hpp>
+#include <print.hpp>
 
-int main() {
-    mylog::info("hunter package works");
-    mylog::warn("this is a warning");
-    return 0;
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+int main(int argc, char* argv[]) {
+  const char* log_path = std::getenv("LOG_PATH");
+  if (log_path == nullptr) {
+    std::cerr << "undefined environment variable: LOG_PATH" << std::endl;
+    return 1;
+  }
+  std::string text;
+  while (std::cin >> text) {
+    std::ofstream out{log_path, std::ios_base::app};
+    print(text, out);
+    out << std::endl;
+  }
 }
